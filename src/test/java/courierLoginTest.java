ce557d9cad5cb9e219b6ef6c6d451d48ae077910
DariaVolkova02047 <120187685+DariaVolkova02047@users.noreply.github.com>
2023-02-16 09:53:46 +0300

    Update courierLoginTest.java

--- src/test/java/courierLoginTest.java
+++ src/test/java/courierLoginTest.java
@@ -5,94 +5,132 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.isA;
+public class CourierTest {
 
-public class CourierLoginTest {
+    private Courier randomCourier;
+    private CourierClient courierClient;
+    private CourierAssertions check;
+    private int courierId;
+
     @Before
     public void setUp() {
-        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru/";
-        Courier courier = new Courier("sashasasha", "sasha", "Sasha");
-        Response response = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(courier)
-                .when()
-                .post("/api/v1/courier");
-
-        response.then().assertThat().body("ok", equalTo(true))
-                .and()
-                .statusCode(201);
+        courierClient = new CourierClient();
+        check = new CourierAssertions();
+        randomCourier = CourierGenerator.getRandom();
     }
 
     @After
-    public void tearDown() {
-        CourierLogin courierLogin = new CourierLogin("sashasasha", "sasha");
+    public void cleanUp() {
+        if (courierId > 0) {
+            courierClient.delete(courierId);
+        }
+    }
 
-        Response responseLogin = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(courierLogin)
-                .when()
-                .post("/api/v1/courier/login");
+    //Courier tests
+    @Test
+    public void courierCanBeCreated() {
+        ValidatableResponse createResponse = courierClient.create(randomCourier);
+        check.createdSuccessfully(createResponse);
 
-        responseLogin.then().assertThat().body("id", isA(Integer.class))
-                .and()
-                .statusCode(200);
-
-        String IdString = responseLogin.body().asString();
-        Gson gson = new Gson();
-        CourierDelete id = gson.fromJson(IdString, CourierDelete.class);
-
-
-        Response responseDelete = given()
-                .header("Content-type", "application/json")
-                .when()
-                .delete(String.format("/api/v1/courier/%s", id.getId()));
-
-        responseDelete.then().assertThat().body("ok", equalTo(true))
-                .and()
-                .statusCode(200);
-
+        courierId = courierClient.login(CourierCredentials.from(randomCourier)).extract().path("id");
     }
 
     @Test
-    public void checkCourierLoginResponseBodyTest() {
-        CourierLogin courierLogin = new CourierLogin("sashasasha", "sasha");
+    public void identicalCourierCanNotBeCreated() {
+        courierClient.create(randomCourier);
+        ValidatableResponse createResponse = courierClient.create(randomCourier);
+        check.creationConflicted(createResponse);
 
-        Response response = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(courierLogin)
-                .when()
-                .post("/api/v1/courier/login");
-
-        response.then().assertThat().body("id", isA(Integer.class))
-                .and()
-                .statusCode(200);
-
-        System.out.println(response.body().asString());
-
+        courierId = courierClient.login(CourierCredentials.from(randomCourier)).extract().path("id");
     }
 
     @Test
-    public void checkCourierLoginBadPasswordResponseBodyTest() {
-        CourierLogin courierLogin = new CourierLogin("sashasasha", "sashasasha");
-
-        Response response = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(courierLogin)
-                .when()
-                .post("/api/v1/courier/login");
-
-        response.then().assertThat().body("message", equalTo("Учетная запись не найдена"))
-                .and()
-                .statusCode(404);
-
-        System.out.println(response.body().asString());
-
+    public void courierWithoutLoginCanNotBeCreated() {
+        randomCourier.setLogin(null);
+        ValidatableResponse createResponse = courierClient.create(randomCourier);
+        check.creationFailed(createResponse);
     }
 
+    @Test
+    public void courierWithoutPasswordCanNotBeCreated() {
+        randomCourier.setPassword(null);
+        ValidatableResponse createResponse = courierClient.create(randomCourier);
+        check.creationFailed(createResponse);
+    }
+
+    @Test
+    public void courierWithoutLastNameCanNotBeCreated() {
+        randomCourier.setLastName(null);
+        ValidatableResponse createResponse = courierClient.create(randomCourier);
+        check.creationFailed(createResponse);
+    }
+
+    @Test
+    public void courierWithBusyLoginCanNotBeCreated() {
+        randomCourier.setLogin("IdenticalLogin");
+        courierClient.create(randomCourier);
+        Courier secondCourier = CourierGenerator.getRandom();
+        secondCourier.setLogin("IdenticalLogin");
+        ValidatableResponse createResponse = courierClient.create(secondCourier);
+        check.creationConflicted(createResponse);
+
+        courierId = courierClient.login(CourierCredentials.from(randomCourier)).extract().path("id");
+    }
+
+    //Login tests
+    @Test
+    public void courierCanLoggedIn() {
+        courierClient.create(randomCourier);
+        CourierCredentials credentials = CourierCredentials.from(randomCourier);
+        ValidatableResponse loginResponse = courierClient.login(credentials);
+        check.loggedInSuccessfully(loginResponse);
+        courierId = loginResponse.extract().path("id");
+    }
+
+    @Test
+    public void loginWithoutLoginFails() {
+        courierClient.create(randomCourier);
+        courierId = courierClient.login(CourierCredentials.from(randomCourier)).extract().path("id");
+        CourierCredentials credentials = CourierCredentials.from(randomCourier);
+        credentials.setLogin(null);
+        ValidatableResponse loginResponse = courierClient.login(credentials);
+        check.loggedInWithoutRequiredFieldFailed(loginResponse);
+    }
+
+    @Test
+    public void loginWithoutPasswordFails() {
+        courierClient.create(randomCourier);
+        courierId = courierClient.login(CourierCredentials.from(randomCourier)).extract().path("id");
+        CourierCredentials credentials = CourierCredentials.from(randomCourier);
+        credentials.setPassword(null);
+        ValidatableResponse loginResponse = courierClient.login(credentials);
+        check.loggedInWithoutRequiredFieldFailed(loginResponse);
+    }
+
+    @Test
+    public void loginWithInvalidLoginFails() {
+        courierClient.create(randomCourier);
+        courierId = courierClient.login(CourierCredentials.from(randomCourier)).extract().path("id");
+        CourierCredentials credentials = CourierCredentials.from(randomCourier);
+        credentials.setLogin("InvalidLogin");
+        ValidatableResponse loginResponse = courierClient.login(credentials);
+        check.loggedInWithInvalidFieldFailed(loginResponse);
+    }
+
+    @Test
+    public void loginWithInvalidPasswordFails() {
+        courierClient.create(randomCourier);
+        courierId = courierClient.login(CourierCredentials.from(randomCourier)).extract().path("id");
+        CourierCredentials credentials = CourierCredentials.from(randomCourier);
+        credentials.setPassword("InvalidPassword");
+        ValidatableResponse loginResponse = courierClient.login(credentials);
+        check.loggedInWithInvalidFieldFailed(loginResponse);
+    }
+
+    @Test
+    public void loginNonexistentCourierFails() {
+        CourierCredentials credentials = CourierCredentials.from(randomCourier);
+        ValidatableResponse loginResponse = courierClient.login(credentials);
+        check.loggedInWithInvalidFieldFailed(loginResponse);
+    }
 }
