4afdbd038e48bb287eeb503fef3939a8040770de
DariaVolkova02047 <120187685+DariaVolkova02047@users.noreply.github.com>
2023-02-27 08:07:04 +0300

    Update courierLoginTest.java

--- src/test/java/courierLoginTest.java
+++ src/test/java/courierLoginTest.java
@@ -10,44 +10,89 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.isA;
 
 public class courierLoginTest {
-
-    @Before("Get response for incorrect password")
-    public Response getIncorrectPasswordResponse(Object body) {
-        return given()
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru/";
+        Courier courier = new Courier("nikitanikita", "nikita", "Nikita");
+        Response response = given()
                 .header("Content-type", "application/json")
                 .and()
-                .body(body)
+                .body(courier)
                 .when()
-                .post("/api/v1/loginCourier");
-        .then();
+                .post("/api/v1/courier");
+
+        response.then().assertThat().body("ok", equalTo(true))
+                .and()
+                .statusCode(201);
     }
 
-    @After("Get response for incorrect password")
-    public Response getIncorrectPasswordResponse(Object body) {
-        return given()
+    @After
+    public void tearDown() {
+        CourierLogin courierLogin = new CourierLogin("nikitanikita", "nikita");
+
+        Response responseLogin = given()
                 .header("Content-type", "application/json")
                 .and()
-                .body(body)
+                .body(courierLogin)
                 .when()
-                .post("/api/v1/login");
-        .then();
+                .post("/api/v1/courier/login");
+
+        responseLogin.then().assertThat().body("id", isA(Integer.class))
+                .and()
+                .statusCode(200);
+
+        String IdString = responseLogin.body().asString();
+        Gson gson = new Gson();
+        CourierDelete id = gson.fromJson(IdString, CourierDelete.class);
+
+
+        Response responseDelete = given()
+                .header("Content-type", "application/json")
+                .when()
+                .delete(String.format("/api/v1/courier/%s", id.getId()));
+
+        responseDelete.then().assertThat().body("ok", equalTo(true))
+                .and()
+                .statusCode(200);
+
     }
 
     @Test
-    @DisplayName("check Courier Login Response Body Test")
-    public void testErrorMessageForIncorrectPassword(){
-        CourierLogin CourierLogin = new CourierLogin();
-        Response incorrectPasswordResponse = CourierLogin.getIncorrectPasswordResponse(new
-                AuthCourier(CourierLogin.login,"sasha WrongPassword Sasha"));
-        incorrectPasswordResponse.statusCode(404).and.assertThat().body("message", is("Учетная запись не найдена"));
+    public void checkCourierLoginResponseBodyTest() {
+        CourierLogin courierLogin = new CourierLogin("nikitanikita", "nikita");
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(courierLogin)
+                .when()
+                .post("/api/v1/courier/login");
+
+        response.then().assertThat().body("id", isA(Integer.class))
+                .and()
+                .statusCode(200);
+
+        System.out.println(response.body().asString());
+
     }
 
     @Test
-    @DisplayName("check Courier Login Bad Password Response Body Test")
-    public void testErrorMessageForIncorrectPassword(){
-        CourierLogin CourierLogin = new CourierLogin();
-        Response incorrectPasswordResponse = CourierLogin.getIncorrectPasswordResponse(new
-                AuthCourier(CourierLogin.login,"*** WrongPassword sasha"));
-        incorrectPasswordResponse.statusCode(404).and.assertThat().body("message", is("Учетная запись не найдена"));
+    public void checkCourierLoginBadPasswordResponseBodyTest() {
+        CourierLogin courierLogin = new CourierLogin("nikitanikita", "nikitanikita");
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(courierLogin)
+                .when()
+                .post("/api/v1/courier/login");
+
+        response.then().assertThat().body("message", equalTo("Учетная запись не найдена"))
+                .and()
+                .statusCode(404);
+
+        System.out.println(response.body().asString());
+
     }
+
 }
